c2fdca4e37426541e40573d6b4233764ea2a815a
geoin-git <geoin.work@gmail.com>
2025-11-13 23:56:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 // Fetches data from GitHub-hosted JSON file (Recommended for hosting)
 
 // GitHub raw JSON file URL
-const DATA_URL = 'https://raw.githubusercontent.com/geoin-git/brick-kilns-dashboard/main/kilns.json';
-//const DATA_URL = '/main/data/kilns.json';
+//const DATA_URL = 'https://raw.githubusercontent.com/geoin-git/brick-kilns-dashboard/main/data/kilns.json';
+const DATA_URL = 'data/kilns.json';
 //const DATA_URL = 'kilns.json';
 
 let map;
@@ -698,6 +698,4 @@ setInterval(function() {
 
 // Initialize on page load
 window.addEventListener('load', init);
-
-
 
